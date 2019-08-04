ac030da5bfb54a7e72a2b443d7ed330d6be1212b
christzefanya <53162335+christzefanya@users.noreply.github.com>
2019-08-04 23:59:47 +0700

    revisi joints

--- trGrafkom.cpp
+++ trGrafkom.cpp
@@ -1,27 +1,23 @@
+// _TugasRancang.cpp : Defines the entry point for the console application.
+//
+
 
 #include "stdafx.h"
 #include "windows.h"
 #include "stdlib.h"
 #include "glut.h"
 #include "Box2D\Box2D.h"
-#include "imageloader.cpp"
-#include "vector"
-
-
-
+#include "imageloader.h"
 
 const float WIDTH = 800.0;
 const float HEIGHT = 600.0;
-
 
 const float m2p = 20; //meter to pixel
 const float p2m = 1 / m2p; //pixel to meter
 const float PI = 3.14;
 
-
 bool mouseDown;
 char keypress = 'a';
-
 
 class MyQueryCallback : public b2QueryCallback
 {
@@ -32,23 +28,16 @@ public:
 		foundBodies.push_back(fixture->GetBody());
 		return true;//keep going to find all fixtures in the query area
 	}
-};
-
-
 
 GLuint _textureId;
-
 
 float32 timeStep = 1 / 60.0;
 int32 velocityIteration = 8.0;
 int32 positionIteration = 3.0;
 
-
 //define physics world
 b2Vec2 gravity(0.0f, 9.8f);
 b2World* world; //pointer, dynamically allocated
-
-
 
 GLuint loadTexture(Image* image) {
 	GLuint textureId;
@@ -58,33 +47,28 @@ GLuint loadTexture(Image* image) {
 	return textureId;
 }
 
-
 b2Body* addRectangle(int x, int y, int w, int h, bool dyn = true){ //add bodydef
-	//create dynamic body (tipe body dinamis yg dapat terpengaruh Force/gravity)
+	//create dynamic body
 	b2BodyDef bodyDef;
 	if (dyn == true) {
 		bodyDef.type = b2_dynamicBody;
 	}
-	bodyDef.position.Set(x*p2m, y*p2m);                                //menetapkan posisi body dibuat -> convert ke meter ( *20)
+	bodyDef.position.Set(x*p2m, y*p2m);
 	b2Body* body = world->CreateBody(&bodyDef);
 
-
 	//attach polygon using fixture def
-	b2PolygonShape dynamicBox;                                                //deklarasi shape dengan nama dynamicBox
-	dynamicBox.SetAsBox(w*p2m / 2, h*p2m / 2);                //deklarasi ukuran box
+	b2PolygonShape dynamicBox;
+	dynamicBox.SetAsBox(w*p2m / 2, h*p2m / 2);
 
-
-	b2FixtureDef fixtureDef;                                                //deklarasi fixture
-	fixtureDef.shape = &dynamicBox;                                        //dengan shape yg dideklarasikan dynamicBox
-	fixtureDef.density = 0.5f;                                                //fixture punya kerapatan 0.5
-	fixtureDef.friction = 0.1f;                                                //gaya gesek permukaan 0.1
-	fixtureDef.restitution = 0.15f;                                        //kelembaman 0.15
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicBox;
+	fixtureDef.density = 0.5f;
+	fixtureDef.friction = 0.1f;
+	fixtureDef.restitution = 0.15f;
 	body->CreateFixture(&fixtureDef);
-
 
 	return body;
 }
-
 
 b2Body* addCircle(int x, int y, int r, bool dyn = true){
 	//create dynamic body
@@ -95,12 +79,10 @@ b2Body* addCircle(int x, int y, int r, bool dyn = true){
 	bodyDef.position.Set(x*p2m, y*p2m);
 	b2Body* body = world->CreateBody(&bodyDef);
 
-
 	//attach circle using fixture def
 	b2CircleShape dynCircle;
 	dynCircle.m_radius = 10 * p2m;
 	dynCircle.m_p.Set(0, 0);
-
 
 	b2FixtureDef fixtureDef;
 	fixtureDef.shape = &dynCircle;
@@ -109,10 +91,8 @@ b2Body* addCircle(int x, int y, int r, bool dyn = true){
 	fixtureDef.restitution = 0.15f;
 	body->CreateFixture(&fixtureDef);
 
-
 	return body;
 }
-
 
 b2Body* add3angle(int x, int y, float r, bool dyn = true){ //add bodydef
 	//create dynamic body
@@ -123,37 +103,32 @@ b2Body* add3angle(int x, int y, float r, bool dyn = true){ //add bodydef
 	bodyDef.position.Set(x*p2m, y*p2m);
 	b2Body* body = world->CreateBody(&bodyDef);
 
-
 	//attach polygon using fixture def
-	b2PolygonShape dyn3angle;                                                                        //polygon dengan bentuk sesuai vertices/titik2 yng ditentukan
+	b2PolygonShape dyn3angle;
 	b2Vec2 Vertices[4];
-	float a = 4 * PI / 3;
+	float a = 12.56 / 3;
 	for (int i = 0; i < 4; i++)
 	{
-		Vertices[i].Set(0 + cos(i*a)*r * 8, 0 + sin(i*a)*r * 8);        //mengisi titik2 sesuai rumus ke array b2vec2 Vertices
+		Vertices[i].Set(0 + cos(i*a)*r * 8, 0 + sin(i*a)*r * 8);
 	}
 	int32 count = 4;
-	dyn3angle.Set(Vertices, count);                                                                //membentuk shape polygon dengan titik2 Vertices dan jumlah titik count.
-
+	dyn3angle.Set(Vertices, count);
 
 	b2FixtureDef fixtureDef;
 	fixtureDef.shape = &dyn3angle;
-	fixtureDef.density = 0.2f;
+	fixtureDef.density = 0.5f;
 	fixtureDef.friction = 0.1f;
-	fixtureDef.restitution = 0.5f;
+	fixtureDef.restitution = 0.15f;
 	body->CreateFixture(&fixtureDef);
-
 
 	return body;
 }
 
-
 void drawSquare(b2Vec2* points, b2Vec2 center, float angle){
-	//menerima parameter pointer "points", "center", dan "angle"
-	glPushMatrix();
-	glTranslatef(center.x* m2p, center.y*m2p, 0);                                                        //geser bentuk ke center.x, dan center.y yg dikonvert ke pixel
-	glRotatef(angle*180.0 / PI, 0, 0, 1);
 
+	glPushMatrix();
+	glTranslatef(center.x* m2p, center.y*m2p, 0);
+	glRotatef(angle*180.0 / PI, 0, 0, 1);
 
 	glEnable(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, _textureId);
@@ -177,8 +152,8 @@ void drawSquare(b2Vec2* points, b2Vec2 center, float angle){
 		else if (i == 3){
 			glTexCoord2f(0.0f, 1.0f);
 		}
-		glVertex2f(points[i].x*m2p, points[i].y*m2p);
-	}
+		glVertex2f(points[i].x*m2p, points[i].y*m2p);                                                //menggambar dengan vertex yang dipass dari points yang berasal dari
+	}                                                                                                                                        //fungsi render.
 	glEnd();
 
 
@@ -191,9 +166,6 @@ void drawSquare(b2Vec2* points, b2Vec2 center, float angle){
 
 
 }
-
-
-
 
 void drawCircle(b2Vec2 center, float r, float angle){
 	glColor3f(1, 1, 1);
@@ -210,7 +182,6 @@ void drawCircle(b2Vec2 center, float r, float angle){
 	glFlush();
 }
 
-
 void draw3angle(b2Vec2* points, b2Vec2 center, float angle){
 	glColor3f(1, 1, 1);
 	glPushMatrix();
@@ -218,13 +189,12 @@ void draw3angle(b2Vec2* points, b2Vec2 center, float angle){
 	glRotatef(angle*180.0 / PI, 0, 0, 1);
 	glBegin(GL_POLYGON);
 	for (int i = 0; i<4; i++){
-		glVertex2d(points[i].x*m2p, points[i].y*m2p);
+		glVertex2f(points[i].x*m2p, points[i].y*m2p);
 	}
 	glEnd();
 	glPopMatrix();
 	glFlush();
 }
-
 
 void render(){
 	b2Body* tmp = world->GetBodyList(); //get all body in the world and store it at tmp pointer
@@ -251,7 +221,6 @@ void render(){
 	world->Step(timeStep, velocityIteration, positionIteration);                        //update frame sesuai gravitasi dan waktu
 }
 
-
 void handleKeypress(unsigned char key, int x, int y) {
 	switch (key) {
 	case 'a':
@@ -267,11 +236,9 @@ void handleKeypress(unsigned char key, int x, int y) {
 		keypress = 'D';
 		break;
 
-
 	}
 	printf("%c", keypress);
 }
-
 
 void mouse(int button, int state, int x, int y)
 {
@@ -279,27 +246,21 @@ void mouse(int button, int state, int x, int y)
 	{
 		mouseDown = true;
 		glColor3f(1, 1, 1);
-		addRectangle(x, y, 50, 50, true);                                                                //menambahkan objek persegi tiap ditekan
+		addRectangle(x, y, 20, 20, true);
 
 
-
-
-		world->Step(timeStep, velocityIteration, positionIteration);        //update frame
-
+		world->Step(timeStep, velocityIteration, positionIteration);
 
 		glutSwapBuffers();
 		glutPostRedisplay();
-
 
 	}
 	else if ((keypress == 's' || keypress == 'S') && button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
 		mouseDown = true;
 		glColor3f(1, 1, 1);
-		addCircle(x, y, 5000, true);
-
+		addCircle(x, y, 2000, true);
 
 		world->Step(timeStep, velocityIteration, positionIteration);
-
 
 		glutSwapBuffers();
 		glutPostRedisplay();
@@ -311,10 +272,7 @@ void mouse(int button, int state, int x, int y)
 		add3angle(x, y, 2, true);
 
 
-
-
 		world->Step(timeStep, velocityIteration, positionIteration);
-
 
 		glutSwapBuffers();
 		glutPostRedisplay();
@@ -323,8 +281,6 @@ void mouse(int button, int state, int x, int y)
 	{
 		mouseDown = false;
 	}
-
-
 
 	MyQueryCallback callback;
 	b2BodyDef bodyDef;
@@ -351,53 +307,60 @@ void mouse(int button, int state, int x, int y)
 	}
 
 
+
 	printf("%d %d %d \n", state, x, y);
 }
-
 
 void display(){
 	glClear(GL_COLOR_BUFFER_BIT);
 	glLoadIdentity();
+	//untuk gambar semua bentuk
+	b2Body* tmp = world->GetBodyList();
+	b2Vec2 points[4];
+	while (tmp){
+		if (tmp->GetFixtureList()->GetShape()->GetType() == 0){
+			b2CircleShape* c = ((b2CircleShape*)tmp->GetFixtureList()->GetShape());
+			drawCircle(tmp->GetWorldCenter(), c->m_radius, tmp->GetAngle());
+		}
+		else {
+			for (int i = 0; i < 4; i++)
+			{
+				points[i] = ((b2PolygonShape*)tmp->GetFixtureList()->GetShape())->GetVertex(i);
+			}
+			drawSquare(points, tmp->GetWorldCenter(), tmp->GetAngle());
+			draw3angle(points, tmp->GetWorldCenter(), tmp->GetAngle());
+		}
+		tmp = tmp->GetNext();
+	}
 
-	//renderer
-	render();
 
-
+	world->Step(timeStep, velocityIteration, positionIteration);
 
 	glutSwapBuffers();
 	glutPostRedisplay();
 
-
 }
-
 
 void init()
 {
 	glMatrixMode(GL_PROJECTION);
 	//menyesuaikan sistem koordinat opengl
 	glOrtho(0, WIDTH, HEIGHT, 0, -1, 1);
+	//gluOrtho2D(0, WIDTH, HEIGHT, 0);
 	glViewport(0, WIDTH, 0, HEIGHT);
 	glMatrixMode(GL_MODELVIEW);
 	glClearColor(0.0, 0.0, 0.0, 1.0);
-
-
-	//deklarasi (membuat) World dengan parameter gravitasi
 	world = new b2World(gravity);
-
 
 	Image* image = loadBMP("wall.bmp");
 	_textureId = loadTexture(image);
 	delete image;
 
-
 	addRectangle(0, 600, 1600, 20, false);
-
-
 
 
 	glutPostRedisplay();
 }
-
 
 int main(int argc, char** argv)
 {
@@ -408,16 +371,18 @@ int main(int argc, char** argv)
 	init();
 	glutDisplayFunc(display);
 	glutMouseFunc(mouse);
-	
 	glutKeyboardFunc(handleKeypress);
-
 
 	world->Step(timeStep, velocityIteration, positionIteration); //update frame
 	glutSwapBuffers();
 
 
-
+	/*
+	glutMotionFunc(mouseMotion);
+	glutReshapeFunc(handleResize);
+	glutTimerFunc(25, update, 0);*/
 
 	glutMainLoop();
 	return 0;
 }
+
